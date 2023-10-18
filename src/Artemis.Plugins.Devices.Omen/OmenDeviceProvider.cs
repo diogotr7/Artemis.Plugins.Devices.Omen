fa4ac7b7cc1d43773d158b43d0b71fd839ec6c9b
Diogo Trindade <diogotr7@gmail.com>
2023-10-18 17:48:32 +0100

    update packages

--- src/Artemis.Plugins.Devices.Omen/OmenDeviceProvider.cs
+++ src/Artemis.Plugins.Devices.Omen/OmenDeviceProvider.cs
@@ -2,28 +2,31 @@
 using Artemis.Core.Services;
 using RGB.NET.Devices.Omen;
 using System.IO;
+using RGB.NET.Core;
 
 namespace Artemis.Plugins.Devices.Omen
 {
     public class OmenDeviceProvider : DeviceProvider
     {
-        private readonly IRgbService _rgbService;
+        private readonly IDeviceService _deviceService;
 
-        public OmenDeviceProvider(IRgbService rgbService) : base(RGB.NET.Devices.Omen.OmenDeviceProvider.Instance)
+        public override IRGBDeviceProvider RgbDeviceProvider => RGB.NET.Devices.Omen.OmenDeviceProvider.Instance;
+
+        public OmenDeviceProvider(IDeviceService deviceService)
         {
-            _rgbService = rgbService;
+            _deviceService = deviceService;
         }
 
         public override void Enable()
         {
             RGB.NET.Devices.Omen.OmenDeviceProvider.PossibleX64NativePaths.Add(Path.Combine(Plugin.Directory.FullName, "x64", "OmenLightingSDK.dll"));
 
-            _rgbService.AddDeviceProvider(RgbDeviceProvider);
+            _deviceService.AddDeviceProvider(this);
         }
 
         public override void Disable()
         {
-            _rgbService.RemoveDeviceProvider(RgbDeviceProvider);
+            _deviceService.RemoveDeviceProvider(this);
             RgbDeviceProvider.Dispose();
         }
     }
